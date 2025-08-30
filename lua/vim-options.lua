300abad8099e3a8a670effd9623d12ae8e5a0a14
mitsuru <sanglam2806@gmail.com>
2025-08-30 10:14:03 +0900

    update vimkeymap exit

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
 vim.cmd("set clipboard=unnamed") -- yank to clipboard MacOS
 vim.cmd("set number")
 vim.cmd("set relativenumber")
