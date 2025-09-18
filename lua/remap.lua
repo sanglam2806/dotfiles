20f487c795b59a616f64c8c083c7cea91133b84e
mitsuru <sanglam2806@gmail.com>
2025-09-19 06:50:50 +0900

    update remap

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,5 +1,6 @@
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-f>", "=ap")
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
